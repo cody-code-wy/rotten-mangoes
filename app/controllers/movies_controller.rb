3a224852bf12c738b354ca1b9b8ab6676fb7ba5e
William Young <william@werl.me>
2016-06-22 16:07:27 -0700

    Add delete capability to movies
    the movie controller can now delete (destroy) movies.
    
    the show view now has links for delete.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,13 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+  # new
+  # edit
+  # create
+  # update
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
 end
