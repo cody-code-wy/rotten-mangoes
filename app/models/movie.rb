4e627b5494ae1b53a02a2a5def4c9e85409f86f1
William Young <william@werl.me>
2016-06-22 20:52:34 -0700

    Add validations to movie model
    Movie now validates that all fields are present.
    Runtime_in_minutes must be an integer,
    and release_date must be in the past.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,21 @@
 class Movie < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :director, presence: true
+  validates :description, presence: true
+  validates :release_date, presence: true
+  validates :poster_image_url, presence: true
+
+  validates :runtime_in_minutes, numericality: {only_integer: true}
+
+  validate :release_date_is_in_past
+
+  private
+
+  def release_date_is_in_past
+    if release_date && release_date >= Date.today
+      errors.add(:release_date, 'Should be in the past')
+    end
+  end
+
 end
