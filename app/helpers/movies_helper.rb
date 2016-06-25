969e7827d9ae8cb3c41ed5132571a5eec11a0e3a
William Young <william@werl.me>
2016-06-25 12:51:56 -0700

    Add validations to movies class helper
    get_classes_for helper method will now add valid or invalid classes to
    form inputs. This will make them show up green or red (materialiez),
    additionally it adds the tooltipped class to invalid fields. Tooltips
    will be added containing the error message.

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,12 @@
 module MoviesHelper
   def get_classes_for(movie, field)
     out = "validate"
+    return out unless movie
+    if movie.errors[field].count > 0
+      out += " invalid tooltipped"
+    else
+      out += " valid"
+    end
     out
   end
 end
